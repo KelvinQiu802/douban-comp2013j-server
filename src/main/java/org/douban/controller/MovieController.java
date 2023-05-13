1c8bdd2ccc77cee40c53d7ad22969a7e904ed349
cnapop <93379413+cnapop@users.noreply.github.com>
2023-05-13 16:27:04 +0800

    feat: api updateScore

--- src/main/java/org/douban/controller/MovieController.java
+++ src/main/java/org/douban/controller/MovieController.java
@@ -31,4 +31,18 @@ public class MovieController {
             ctx.result("Invalid Input Number").status(400);
         }
     }
+
+    public void movieUpdateScore(Context ctx) {
+        try {
+            String idStr = ctx.pathParam("id");
+            int id = Integer.parseInt(idStr);
+            String scoreStr = ctx.pathParam("score");
+            double score = Double.parseDouble(scoreStr);
+            ctx.json(movieDao.updateMovieScoreById(id, score));
+        } catch (SQLException e) {
+            ctx.result("Internal Server Error").status(500);
+        } catch (NumberFormatException e) {
+            ctx.result("Invalid Input Number").status(400);
+        }
+    }
 }

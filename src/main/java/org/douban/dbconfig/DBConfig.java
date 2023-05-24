62ce94e5a6baa5b0c601616158c4adf0bc8dbc54
KelvinQiu <kelvinqiu802@outlook.com>
2023-05-24 22:03:16 +0800

    feat: use local db

--- src/main/java/org/douban/dbconfig/DBConfig.java
+++ src/main/java/org/douban/dbconfig/DBConfig.java
@@ -4,8 +4,8 @@ public class DBConfig {
     /**
      * Those values are secrets! Do not share it with others!
      */
-    public final static String host = "81.70.146.18";
+    public final static String host = "127.0.0.1";
     public final static String dbname = "douban";
-    public final static String userName = "douban";
-    public final static String password = "NZpYSjmcKm35mfin";
+    public final static String userName = "root";
+    public final static String password = "";
 }
